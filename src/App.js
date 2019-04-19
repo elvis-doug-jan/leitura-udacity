52c8af2614cbffd595814b1e7519a784865cc387
Elvis Douglas Janegitz <edjelvis@gmail.com>
2019-04-19 19:07:13 -0300

    Inciando component post

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Button, Row, Col } from 'react-bootstrap'
 import { Router, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Toolbar from './components/Toolbar'
+import Posts from './components/Posts'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Toolbar />
+        <Posts />
       </div>
     );
   }
