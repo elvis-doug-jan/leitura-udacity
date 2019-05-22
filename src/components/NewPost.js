f6bdf05e25c15971596f08ada87a2750cc06da94
Elvis Douglas Janegitz <edjelvis@gmail.com>
2019-05-21 22:18:08 -0300

    Estilo tela novo post

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { newPost } from './../actions/posts'
-import { Container, Col, Button } from 'react-bootstrap'
+import { Container, Row, Button, Card, Col } from 'react-bootstrap'
 import { getAllCategories } from './../utils/ApiCalls'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import { FaSave } from 'react-icons/fa'
 import { v4 } from 'uuid'
-import './../styles/Post.css'
+import './../styles/NewPost.css'
 
 class NewPost extends Component {
 
@@ -48,21 +49,35 @@ class NewPost extends Component {
   render() {
     return (
       <Container>
-        <Col>
-          <h3>New post</h3>
-          Title:
-          <input value={this.state.title} onChange={e => this.setTitlePost(e.target.value)} />
-          <br />
-          <br />
-          Content:
-          <textarea value={this.state.body} onChange={e => this.setBodyPost(e.target.value)}></textarea>
-          <br />
-          {this.props.categories.map(category => (
-            <Button variant="secondary" key={category.path} onClick={() => this.setCategory(category.name)} >{category.name}</Button>
-          ))}
-          <br />
-          <Button onClick={() => this.createPost()}>Create Post</Button>
-        </Col>
+        <Row className="justify-content-md-center">
+          <Card className="cardContentNewPost">
+            <Row className="justify-content-md-center">
+              <h4>New post</h4>
+            </Row>
+            Title:
+          <input value={this.state.title} onChange={e => this.setTitlePost(e.target.value)} className="inputTitle" />
+            <br />
+            <br />
+            Content:
+          <textarea value={this.state.body} onChange={e => this.setBodyPost(e.target.value)} className="inputContent"></textarea>
+            <br />
+            <Row className="justify-content-md-center">
+              {this.props.categories.map(category => (
+                <Button variant="secondary" key={category.path} onClick={() => this.setCategory(category.name)} className="mr-1 ml-1">
+                  {category.name}
+                </Button>
+              ))}
+            </Row>
+            <br />
+            <Row className="justify-content-md-center">
+              <Col md={2}>
+                <Button onClick={() => this.createPost()} className="buttonCreatePost">
+                  <FaSave />
+                </Button>
+              </Col>
+            </Row>
+          </Card>
+        </Row>
       </Container>
     )
   }
